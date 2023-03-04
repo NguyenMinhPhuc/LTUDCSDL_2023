aeb7c6f26310923d44c34a3a3883e7c99f078aeb
Nguyen Minh Phuc <nmphuc@outlook.com>
2023-03-04 16:31:02 +0700

    buoi học 230304

--- Pro_QuanLyBanHang2023/Pro_QuanLyBanHang2023/BusinessLayer/BLL_SanPham.cs
+++ Pro_QuanLyBanHang2023/Pro_QuanLyBanHang2023/BusinessLayer/BLL_SanPham.cs
@@ -1,6 +1,8 @@
 ﻿using DataLayer;
+using Pro_QuanLyBanHang2023.DTO;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -24,6 +26,29 @@ namespace Pro_QuanLyBanHang2023.BusinessLayer
         public object LayMaxIDSanPham(ref string err)
         {
             return database.MyExcuteScalar(ref err, "PSP_SanPham_MaxID", System.Data.CommandType.StoredProcedure, null);
+        }
+
+        public DataTable LayDanhSachSanPhamAllOrByID(ref string err,string maSP)
+        {
+            SqlParameter[] sqlParameters = new SqlParameter[]
+            {
+                  new SqlParameter("@MaSP", maSP)
+            };
+            return database.LayDuLieuTheoDangBang(ref err, "PSP_SanPham_Select230204", CommandType.StoredProcedure, sqlParameters
+              );
+        }
+        public DataTable LayDonViTinhCbo(ref string err)
+        {
+            return database.LayDuLieuTheoDangBang(ref err, "PSP_DonViTinh_SelectCbo230304", CommandType.StoredProcedure, null);
+        }
+
+        public int InsertSanPham(ref string err, SanPham sanPham)
+        {
+            return database.MyExcuteNonQuery(ref err, "PSP_SanPham_InsertAndUpdate230304", CommandType.StoredProcedure,
+                new SqlParameter("@MaSP", sanPham.MaSP),
+                new SqlParameter("@TenSP", sanPham.TenSP),
+                new SqlParameter("@SoTon", sanPham.SoTon),
+                new SqlParameter("@MaDVT", sanPham.MaDVT));
         }
     }
 }
--- Pro_QuanLyBanHang2023/Pro_QuanLyBanHang2023/Frm_SanPham.cs
+++ Pro_QuanLyBanHang2023/Pro_QuanLyBanHang2023/Frm_SanPham.cs
@@ -1,4 +1,5 @@
 ﻿using Pro_QuanLyBanHang2023.BusinessLayer;
+using Pro_QuanLyBanHang2023.DTO;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -20,29 +21,56 @@ namespace Pro_QuanLyBanHang2023
         }
         BLL_SanPham bd;
         string err = string.Empty;
+        DataTable dataTable;
         private void Frm_SanPham_Load(object sender, EventArgs e)
         {
             bd = new BLL_SanPham();
+            HienThiDanhSachSanPham();
+            HienThiCboDonViTinh();
         }
+
+        private void HienThiCboDonViTinh()
+        {
+            DataTable dt = new DataTable();
+            dt = bd.LayDonViTinhCbo(ref err);
+            cboDonViTinh.DataSource = dt;
+
+            cboDonViTinh.DisplayMember = "TenDVT";
+            cboDonViTinh.ValueMember = "MaDVT";
+
+            cboDonViTinh.SelectedIndex = -1;
+            cboDonViTinh.Text = "Chon don vi tinh";
+        }
+
+        private void HienThiDanhSachSanPham()
+        {
+            //b1: Khai bao doi tuong DataTable
+            dataTable = new DataTable();
+            //b2:Goi du lieu tu bussinessLayer
+            dataTable = bd.LayDanhSachSanPhamAllOrByID(ref err, "0");
+            //B3:gan datatable vao datagridview
+            dgvDanhSachSanPham.DataSource = dataTable.DefaultView;
+        }
+
         int countSP = 0;
         private void btnLaySanPham_Click(object sender, EventArgs e)
         {
-            SqlDataReader sqlDataReader = bd.LayDanhSachSanPham(ref err, "0");
-            if(sqlDataReader!=null)
-            {
-                while(sqlDataReader.Read())
-                {
-                    string line = string.Format("{0}-{1}-{2}", sqlDataReader["MaSP"], sqlDataReader["TenSP"], sqlDataReader["SoTon"]);
-                    lbDanhSachSanPham.Items.Add(line);
-                    countSP++;
-                }
-                lblTongSoLuong.Text = string.Format("Tổng số lượng: {0}", countSP);
-            }    
+            //SqlDataReader sqlDataReader = bd.LayDanhSachSanPham(ref err, "0");
+            //if(sqlDataReader!=null)
+            //{
+            //    while(sqlDataReader.Read())
+            //    {
+            //        string line = string.Format("{0}-{1}-{2}", sqlDataReader["MaSP"], sqlDataReader["TenSP"], sqlDataReader["SoTon"]);
+            //        lbDanhSachSanPham.Items.Add(line);
+            //        countSP++;
+            //    }
+            //    lblTongSoLuong.Text = string.Format("Tổng số lượng: {0}", countSP);
+            //}    
         }
 
         private void btnThemSP_Click(object sender, EventArgs e)
         {
-            txtMaSP.Text = TaoMaSPTuDong();
+           // txtMaSP.Text = TaoMaSPTuDong();
         }
 
         private string TaoMaSPTuDong()
@@ -52,6 +80,57 @@ namespace Pro_QuanLyBanHang2023
             maSP = string.Format("SP{0:0000000}",maxID+1);
 
             return maSP;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            txtMaSP.Text = TaoMaSPTuDong();
+            txtTenSP.Focus();
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            //kiem tra du lieu
+            if (LuuDuLieuSanPham(txtMaSP.Text, txtTenSP.Text, txtSoTon.Text, cboDonViTinh.SelectedValue.ToString()))
+            {
+                HienThiDanhSachSanPham();
+            }
+            
+        }
+
+        private bool LuuDuLieuSanPham(string maSP, string tenSP, string soTon, string maDVT)
+        {
+            SanPham sanPham = new SanPham()
+            {
+                MaSP = maSP,
+                TenSP = tenSP,
+                SoTon = Convert.ToInt32(soTon),
+                MaDVT=Convert.ToInt32(maDVT)
+            };
+            if (bd.InsertSanPham(ref err, sanPham) > 0)
+                return true;
+            return false;
+        }
+        SanPham sanPham;
+        private void dgvDanhSachSanPham_Click(object sender, EventArgs e)
+        {
+            sanPham = new SanPham();
+            if (dgvDanhSachSanPham.Rows.Count > 0)
+            {
+                sanPham.MaSP = dgvDanhSachSanPham.CurrentRow.Cells["colMaSP"].Value.ToString();
+                sanPham.TenSP = dgvDanhSachSanPham.CurrentRow.Cells["colTenSP"].Value.ToString();
+                sanPham.SoTon = Convert.ToInt32(dgvDanhSachSanPham.CurrentRow.Cells["colSoTon"].Value.ToString());
+                sanPham.MaDVT = Convert.ToInt32(dgvDanhSachSanPham.CurrentRow.Cells["colMaDVT"].Value.ToString());
+            }
+            BingSanPhamToControl();
+        }
+
+        private void BingSanPhamToControl()
+        {
+            txtMaSP.Text = sanPham.MaSP;
+            txtTenSP.Text = sanPham.TenSP;
+            txtSoTon.Text = sanPham.SoTon.ToString();
+            cboDonViTinh.SelectedValue = sanPham.MaDVT;
         }
     }
 }
